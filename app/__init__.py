23ab2051ed0bbe3d2fc632322ebaa6481fd47f4b
Yu-Ting, Chou <r10941128@g.ntu.edu.tw>
2024-02-28 01:36:59 +0800

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+import app.model as model
+import numpy as np
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/test', methods=['GET'])
+def getResult():
+    input = np.array([[5.5]])
+    result =model.predict(input)
+    return jsonify({'result':str(result)})
+
+@app.route('/predict', methods=['POST'])
+def postInput():
+    # 取得前端傳來的數值
+    insertValues = request.get_json()
+    x=insertValues['x']
+    input = np.array([[x]])
+    # 預測
+    result = model.predict(input)
+
+    return jsonify({'result': str(result)})
